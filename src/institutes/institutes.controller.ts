9272658c34cd432498a12e89bef0c33ee4606fbb
marius-personal <mariusjacobhernandez03@gmail.com>
2024-08-21 18:57:00 +0800

    feat - all endpoint in institutes done

--- src/institutes/institutes.controller.ts
+++ src/institutes/institutes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, ValidationPipe, ParseUUIDPipe } from '@nestjs/common';
 import { InstitutesService } from './institutes.service';
 import { CreateInstituteDto } from 'src/institutes/dto/create-institutes.dto';
 import { UpdateInstituteDto } from 'src/institutes/dto/update-institutes.dto';
@@ -7,25 +7,25 @@ import { UpdateInstituteDto } from 'src/institutes/dto/update-institutes.dto';
 export class InstitutesController {
   constructor(private readonly institutesService: InstitutesService) {}
 
-  @Get()
+  @Get() // done
   getInstitutes() {
     return this.institutesService.getInstitutes();
   }
-  @Post()
+  @Post() // done
   createInstitutes(@Body(ValidationPipe) institute: CreateInstituteDto) {
     return this.institutesService.createInstitutes(institute);
   }
 
-  @Get(':uuid')
-  getInstitute(@Param('uuid') uuid: string) {
+  @Get(':uuid') //done
+  getInstitute(@Param('uuid', ParseUUIDPipe) uuid: string) {
     return this.institutesService.getInstitute(uuid);
   }
-  @Patch(':uuid')
+  @Patch(':uuid') // done
   updateInstitute(@Param('uuid') uuid: string, @Body() institute: UpdateInstituteDto) {
     return this.institutesService.updateInstitute(uuid, institute);
   }
 
-  @Delete(':uuid')
+  @Delete(':uuid') //done
   deleteInstitute(@Param('uuid') uuid: string) {
     return this.institutesService.deleteInstitute(uuid);
   }
