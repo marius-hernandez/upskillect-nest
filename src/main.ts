58c3b7a53809e8931ce97b4094c63654165a5196
marius-personal <mariusjacobhernandez03@gmail.com>
2024-10-13 21:51:46 +0800

    chore - api versioning setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+
+  // versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix:'api/v',
+  })
+
   await app.listen(3001);
 }
 bootstrap();
